21983ad82da632a5feb01494de51a3385d9a26f4
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-06-17 12:30:29 -0700

    App class and more webpack config

---
+++ lib/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <h1>React is kinda working</h1>;
+    }
+}
+
+export default App;
---
+++ lib/entry.jsx
@@ -0,0 +1,18 @@
+import AOS from 'aos';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+document.addEventListener("DOMContentLoaded", function() {
+  // init animate on scroll
+  AOS.init();
+  // init smooth scrolling
+  $.srSmoothscroll({
+    step: 90,
+    speed: 300,
+    ease: 'linear'
+  });
+
+  const root = document.getElementById("root");
+  ReactDOM.render(<App/>, root);
+});
