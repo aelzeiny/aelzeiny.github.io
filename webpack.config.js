ad469567d0ec308713dc8d27a82d11a9b9817513
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-06-17 12:15:30 -0700

    Webpack and module config

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,20 @@ module.exports = {
     path: path.join(__dirname),
     filename: "bundle.js",
   },
-  devtool: "source-maps"
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
 };
