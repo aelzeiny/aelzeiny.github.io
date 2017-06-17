13798aad766839495fc294a6a1a8d21e1aa70e4f
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-06-17 15:10:04 -0700

    Add logos for skills

--- lib/components/skills.jsx
+++ lib/components/skills.jsx
@@ -9,26 +9,26 @@ class Skills extends React.Component {
         return (
             <section className="skills">
                 <div className="frontend">
-                    <img className="skill" src="assets/react.jpg"></img>
-                    <img className="skill" src="assets/redux.png"></img>
-                    <img className="skill" src="assets/html-5.png"></img>
-                    <img className="skill" src="assets/sass.png"></img>
-                    <img className="skill" src="assets/d3.png"></img>
-                    <img className="skill" src="assets/jquery.png"></img>
-                    <img className="skill" src="assets/bootstrap-4.svg"></img>
+                    <img className="skill" src="assets/coding_icons/react.jpg"></img>
+                    <img className="skill" src="assets/coding_icons/redux.png"></img>
+                    <img className="skill" src="assets/coding_icons/html-5.png"></img>
+                    <img className="skill" src="assets/coding_icons/sass.png"></img>
+                    <img className="skill" src="assets/coding_icons/d3.png"></img>
+                    <img className="skill" src="assets/coding_icons/jquery.png"></img>
+                    <img className="skill" src="assets/coding_icons/bootstrap-4.svg"></img>
                 </div>
                 <div className="backend">
-                    <img className="skill" src="assets/rails.png"></img>
-                    <img className="skill" src="assets/asp-net.png"></img>
-                    <img className="skill" src="assets/git.png"></img>
-                    <img className="skill" src="assets/nodejs.svg"></img>
-                    <img className="skill" src="assets/webpack.svg"></img>
+                    <img className="skill" src="assets/coding_icons/rails.png"></img>
+                    <img className="skill" src="assets/coding_icons/asp-net.png"></img>
+                    <img className="skill" src="assets/coding_icons/git.png"></img>
+                    <img className="skill" src="assets/coding_icons/nodejs.svg"></img>
+                    <img className="skill" src="assets/coding_icons/webpack.svg"></img>
                 </div>
                 <div className="language">
-                    <img className="skill" src="assets/ruby.png"></img>
-                    <img className="skill" src="assets/ruby.png"></img>
-                    <img className="skill" src="assets/csharp.svg"></img>
-                    <img className="skill" src="assets/java.png"></img>
+                    <img className="skill" src="assets/coding_icons/csharp.svg"></img>
+                    <img className="skill" src="assets/coding_icons/java.png"></img>
+                    <img className="skill" src="assets/coding_icons/ruby.png"></img>
+                    <img className="skill" src="assets/coding_icons/javascript.png"></img>
                 </div>
             </section>  
         );
