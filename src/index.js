1e480f1af83399917e88984f0c0db35407f6cd1c
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-06-18 09:07:18 -0700

    Delete boilerplate react components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
+import AOS from 'aos';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/App';
+import App from './components/app';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
-);
+require('../style/application.scss');
+require('../style/projects.scss');
+require('../style/skills.scss');
+
+document.addEventListener("DOMContentLoaded", function() {
+  // init animate on scroll
+  AOS.init();
+  // init smooth scrolling
+  $.srSmoothscroll({
+    step: 90,
+    speed: 300,
+    ease: 'linear'
+  });
+
+  const root = document.getElementById("root");
+  ReactDOM.render(<App/>, root);
+});
