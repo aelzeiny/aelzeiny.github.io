771229c00843bc54e610e91f41efc5d5b730a5b2
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-06-18 19:28:39 -0700

    Tweeked skills and skill colors

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -82,17 +82,22 @@ class Skills extends React.Component {
     buildSkills(data) {
         /*eslint-enable indent*/
         /*global d3*/
-        let svg = d3.select('#skills-svg');
-        let width = svg.attr('width'); // get width in pixels
+        let svg = d3.select(this.svg);
+        let width =svg.node().getBoundingClientRect().width; // get width in pixels
         let height = svg.attr('height');
         let centerX = width * 0.5;
         let centerY = height * 0.5;
-        let strength = 0.05;
+        let strength = 0.15;
         let focusedNode;
 
         let format = d3.format(',d');
-
-        let scaleColor = d3.scaleOrdinal(d3.schemeCategory20);
+        let allCats = {};
+        const colors = ['#bd589e', '#6b3d71', '#ffa4e3', 'blue'];
+        let scaleColor = (cat) => {
+            if(!allCats[cat])
+                allCats[cat] = colors[Object.keys(allCats).length];
+            return allCats[cat];
+        };
 
         // use pack to calculate radius of the circle
         let pack = d3.pack()
@@ -107,8 +112,8 @@ class Skills extends React.Component {
             .force('charge', d3.forceManyBody())
             .force('collide', forceCollide)
             // .force('center', d3.forceCenter(centerX, centerY))
-            .force('x', d3.forceX(centerX ).strength(strength))
-            .force('y', d3.forceY(centerY ).strength(strength));
+            .force('x', d3.forceX(centerX).strength(strength))
+            .force('y', d3.forceY(centerY).strength(strength));
 
         // reduce number of circles on mobile screen due to slow computation
         if ('matchMedia' in window && window.matchMedia('(max-device-width: 767px)').matches) {
@@ -139,7 +144,6 @@ class Skills extends React.Component {
             };
         });
 
-        svg.style('background-color', '#eee');
         let node = svg.selectAll('.node')
             .data(nodes)
             .enter().append('g')
@@ -211,137 +215,14 @@ class Skills extends React.Component {
 
         node.append('title')
             .text(d => (d.cat + '::' + d.name + '\n' + format(d.value)));
-
-        node.on('click', (currentNode) => {
-            d3.event.stopPropagation();
-            console.log('currentNode', currentNode);
-            let currentTarget = d3.event.currentTarget; // the <g> el
-
-            if (currentNode === focusedNode) {
-                // no focusedNode or same focused node is clicked
-                return;
-            }
-            let lastNode = focusedNode;
-            focusedNode = currentNode;
-
-            simulation.alphaTarget(0.2).restart();
-            // hide all circle-overlay
-            d3.selectAll('.circle-overlay').classed('hidden', true);
-            d3.selectAll('.node-icon').classed('node-icon--faded', false);
-
-            // don't fix last node to center anymore
-            if (lastNode) {
-                lastNode.fx = null;
-                lastNode.fy = null;
-                node.filter((d, i) => i === lastNode.index)
-                    .transition().duration(2000).ease(d3.easePolyOut)
-                    .tween('circleOut', () => {
-                        let irl = d3.interpolateNumber(lastNode.r, lastNode.radius);
-                        return (t) => {
-                            lastNode.r = irl(t);
-                        };
-                    })
-                    .on('interrupt', () => {
-                        lastNode.r = lastNode.radius;
-                    });
-            }
-
-            // if (!d3.event.active) simulation.alphaTarget(0.5).restart();
-
-            d3.transition().duration(2000).ease(d3.easePolyOut)
-                .tween('moveIn', () => {
-                    console.log('tweenMoveIn', currentNode);
-                    let ix = d3.interpolateNumber(currentNode.x, centerX);
-                    let iy = d3.interpolateNumber(currentNode.y, centerY);
-                    let ir = d3.interpolateNumber(currentNode.r, centerY * 0.5);
-                    return function (t) {
-                        // console.log('i', ix(t), iy(t));
-                        currentNode.fx = ix(t);
-                        currentNode.fy = iy(t);
-                        currentNode.r = ir(t);
-                        simulation.force('collide', forceCollide);
-                    };
-                })
-                .on('end', () => {
-                    simulation.alphaTarget(0);
-                    let $currentGroup = d3.select(currentTarget);
-                    $currentGroup.select('.circle-overlay')
-                        .classed('hidden', false);
-                    $currentGroup.select('.node-icon')
-                        .classed('node-icon--faded', true);
-
-                })
-                .on('interrupt', () => {
-                    console.log('move interrupt', currentNode);
-                    currentNode.fx = null;
-                    currentNode.fy = null;
-                    simulation.alphaTarget(0);
-                });
-
-        });
-
-        // blur
-        d3.select(document).on('click', () => {
-            let target = d3.event.target;
-            // check if click on document but not on the circle overlay
-            if (!target.closest('#circle-overlay') && focusedNode) {
-                focusedNode.fx = null;
-                focusedNode.fy = null;
-                simulation.alphaTarget(0.2).restart();
-                d3.transition().duration(2000).ease(d3.easePolyOut)
-                    .tween('moveOut', function () {
-                        console.log('tweenMoveOut', focusedNode);
-                        let ir = d3.interpolateNumber(focusedNode.r, focusedNode.radius);
-                        return function (t) {
-                            focusedNode.r = ir(t);
-                            simulation.force('collide', forceCollide);
-                        };
-                    })
-                    .on('end', () => {
-                        focusedNode = null;
-                        simulation.alphaTarget(0);
-                    })
-                    .on('interrupt', () => {
-                        simulation.alphaTarget(0);
-                    });
-
-                // hide all circle-overlay
-                d3.selectAll('.circle-overlay').classed('hidden', true);
-                d3.selectAll('.node-icon').classed('node-icon--faded', false);
-            }
-        });
     }
 
 
     render() {
         return (
             <section className="skills">
-                <svg id="skills-svg" width="500" height="500">
-
-                </svg>
-                {/*<h2>Skills</h2>
-                <div className="frontend">
-                    <img className="skill" src="assets/coding_icons/react.jpg"></img>
-                    <img className="skill" src="assets/coding_icons/redux.png"></img>
-                    <img className="skill" src="assets/coding_icons/html-5.png"></img>
-                    <img className="skill" src="assets/coding_icons/sass.png"></img>
-                    <img className="skill" src="assets/coding_icons/d3.png"></img>
-                    <img className="skill" src="assets/coding_icons/jquery.png"></img>
-                    <img className="skill" src="assets/coding_icons/bootstrap-4.svg"></img>
-                </div>
-                <div className="backend">
-                    <img className="skill" src="assets/coding_icons/rails.png"></img>
-                    <img className="skill" src="assets/coding_icons/asp-net.png"></img>
-                    <img className="skill" src="assets/coding_icons/git.png"></img>
-                    <img className="skill" src="assets/coding_icons/nodejs.svg"></img>
-                    <img className="skill" src="assets/coding_icons/webpack.svg"></img>
-                </div>
-                <div className="language">
-                    <img className="skill" src="assets/coding_icons/csharp.svg"></img>
-                    <img className="skill" src="assets/coding_icons/java.png"></img>
-                    <img className="skill" src="assets/coding_icons/ruby.png"></img>
-                    <img className="skill" src="assets/coding_icons/javascript.png"></img>
-                </div>*/}
+                <h2>Skills</h2>
+                <svg id="skills-svg" ref={(el) => this.svg = el} width="100%" height="500"></svg>
             </section>  
         );
     }
