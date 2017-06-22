8ccb1cc7b190a80bf10bde6645af10be814e1792
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-06-22 11:17:38 -0700

    CloudCasts Project

--- src/components/project_modal.jsx
+++ src/components/project_modal.jsx
@@ -32,7 +32,7 @@ class ProjectModal extends React.Component {
   componentWillReceiveProps(newProps) {
     console.log("PROPS: ", this.props.match.params.projectName, newProps.match.params.projectName);
     if (newProps.match.params.projectName) {
-      const data = this.projects.find((el) => newProps.match.params.projectName.indexOf(el.search) >= 0);
+      const data = this.projects.find((el) => newProps.match.params.projectName.toLowerCase().indexOf(el.search) >= 0);
       this.maximizeModal();
     }
   }
@@ -61,16 +61,33 @@ class ProjectModal extends React.Component {
     this.projects = [
       {
         name: 'Cloud Casts',
-        search: '',
-        image: 'cloudcasts.gif',
-        description: '',
-        github: '',
-        live: '',
+        search: 'cloudcasts',
+        source: 'Cloudcasts.mp4',
+        description: (
+          "CloudCasts is a podcast streaming app featuring original UI and mobile-responsive design. " +
+          "This app pull from a variety of podcast directories (including ITunes) and RSS feeds to sort podcasts based on popularity. " +
+          "Listen to any and every podcast on the web, all from a single source."
+        ),
+        github: 'https://github.com/aelzeiny/CloudCasts',
+        live: 'http://www.cloudcast.life',
         features: [
-          
+          "Live-data fetching from a variety of sources and RSS feeds",
+          "Podcast searching",
+          "Lazy database caching on the backend",
+          "Mobile-Responsive",
+          "Dynamically changing color scheme through image parsing"
         ],
         tech: [
-
+          "javascript",
+          "ruby",
+          "rails",
+          "react",
+          "redux",
+          "heroku",
+          "html5",
+          "css3",
+          "sass",
+          "webpack"
         ]
       }
     ];
