9852bec02ea71ee3eac81341f2326554c42c7f59
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-06-23 13:38:26 -0700

    Added optimized svgs and more layers

--- src/components/parallax.jsx
+++ src/components/parallax.jsx
@@ -33,19 +33,20 @@ class Parallax extends React.Component {
     render() {
         return (
             <figcaption>
-                <div className="layer parallax" id="layer-0" data-parallax-ratio="3"></div>
+                <div className="layer parallax" id="layer-8" data-parallax-ratio="3"></div>
                 <div className="layer parallax" data-parallax-ratio="3">
                     <h1>Ahmed Elzeiny</h1>
                     <h2>Software Engineer</h2>
                     <p className="cursive">In the bay area</p>
                 </div>
-                <div className="layer parallax" id="layer-1" data-parallax-ratio="29"></div>
-                <div className="layer parallax" id="layer-2" data-parallax-ratio="30"></div>
-                <div className="layer parallax" id="layer-3" data-parallax-ratio="40"></div>
+                <div className="layer parallax" id="layer-7" data-parallax-ratio="29"></div>
+                <div className="layer parallax" id="layer-6" data-parallax-ratio="30"></div>
+                <div className="layer parallax" id="layer-5" data-parallax-ratio="40"></div>
                 <div className="layer parallax" id="layer-4" data-parallax-ratio="45"></div>
-                <div className="layer parallax" id="layer-4-5" data-parallax-ratio="55"></div> 
-                <div className="layer parallax" id="layer-5" data-parallax-ratio="70"></div>
-                <div className="layer" id="layer-6"></div>
+                <div className="layer parallax" id="layer-3" data-parallax-ratio="50"></div>
+                <div className="layer parallax" id="layer-2" data-parallax-ratio="55"></div> 
+                <div className="layer parallax" id="layer-1" data-parallax-ratio="70"></div>
+                <div className="layer" id="layer-0"></div>
                 {/*<div className="layer" id="base"></div>*/}
             </figcaption>
         );
