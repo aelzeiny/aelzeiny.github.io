cbf39a5b59ebf917dbaddc920e93ce39329ea3e7
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-07-05 21:21:12 -0700

    Remove compilation bug

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -17,7 +17,7 @@ class About extends React.Component {
               It’s time for me to finally admit it out loud - <b>I have a coding problem</b>. In college, my <b>passion</b> for
               coding had <b>manifested</b> itself into a CS minor.
               In between my rigorous engineering courses, I always had one or two <b>spare</b> projects; even during finals week.
-              The pinnacle of my <b>epidemic</b> transpired in my last occupation, when I became a full-stack engineer by <b>day</b> and structural
+              The pinnacle of my <b>epidemic</b> transpired in my last occupation,when I became a full-stack engineer by <b>day</b> and structural
               Engineer-In-Training (E.I.T) by <b>late afternoon</b>.
             </p>
             <p>
@@ -32,5 +32,3 @@ class About extends React.Component {
 }
 
 export default About;
-It’s time for me to finally admit it out loud - I have a coding problem. In college, my passion for coding had manifested itself into a CS minor. In between my rigorous engineering courses, I always had one or two spare projects; even during finals week. The pinnacle of my epidemic transpired in my last occupation, when I became a full-stack engineer by day and structural Engineer-In-Training (E.I.T) by late afternoon.
- Finally, after 6 years of programming, I no longer have a coding problem; I now have a software-engineering career.
