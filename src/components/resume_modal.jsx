43e02bfdf9c15514d3641df28cf5c3a34cbe451e
Ahmed Elzeiny <alfred.e@leantaas.com>
2020-03-03 22:37:41 -0800

    Update Resume

--- src/components/resume_modal.jsx
+++ src/components/resume_modal.jsx
@@ -12,7 +12,7 @@ class ResumeModal extends ModalComponent {
     return (
         <div className="resume">
           <div className="download">
-          
+
             <p>This portfolio is made<br/> in React</p><br/>
             <p>This resume is 100%<br/>HTML and CSS</p><br/>
             <a href="assets/projects/Elzeiny_Resume_2017.pdf" download><i className="fa fa-file-pdf-o fa-6"></i><br/>PDF Download</a>
@@ -56,144 +56,193 @@ class ResumeModal extends ModalComponent {
             </header>
             <div className="body">
               <nav>
-                <section>
-                  <h2>EDUCATION</h2>
-                  <h3 className="edu">San Jose State University</h3>
-                  <div className="indent">
-                    <p>B.S Civil Engineering</p>
-                    <p>Minor Comp Sci</p>
-                  </div>
-                  <h3>App Academy</h3>
-                  <div className="indent">
-                    <p>Web-Development</p>
-                    <p>Mar 2017 - Jun 2017</p>
-                  </div>
-                </section>
-                <section className="borderless">
+                <section className="skills-section">
                   <h2>SKILLS</h2>
-                  <h3>FRONT-END</h3>
+                  <h3>Data</h3>
                   <ul className="skills">
-                    <li><p>React </p><div className="star-ranking"><div className="star-5">★★★★★</div></div></li>
-                    <li><p>Redux</p><div className="star-ranking"><div className="star-5">★★★★★</div></div></li>
-                    <li><p>Vue</p><div className="star-ranking"><div className="star-4-5">★★★★★</div></div></li>
-                    <li><p>CSS/SASS</p><div className="star-ranking"><div className="star-5">★★★★★</div></div></li>
-                    <li><p>HTML 5</p><div className="star-ranking"><div className="star-5">★★★★★</div></div></li>
-                    <li><p>jQuery</p><div className="star-ranking"><div className="star-5">★★★★★</div></div></li>
-                    <li><p>Bootstrap</p><div className="star-ranking"><div className="star-5">★★★★★</div></div></li>
-                    <li><p>D3</p><div className="star-ranking"><div className="star-4">★★★★★</div></div></li>
+                    <li><p>Airflow</p>
+                      <div className="star-ranking"></div>
+                    </li>
+                    <li><p>Kafka</p>
+                      <div className="star-ranking"></div>
+                    </li>
+                    <li><p>Spark</p>
+                      <div className="star-ranking"></div>
+                    </li>
+                    <li><p>Hadoop Ecosystem</p>
+                      <div className="star-ranking"></div>
+                    </li>
                   </ul>
-                  <h3>BACK-END</h3>
+                  <h3>Backend</h3>
                   <ul className="skills">
-                    <li><p>Ruby on Rails</p><div className="star-ranking"><div className="star-5">★★★★★</div></div></li>
-                    <li><p>PostgreSQL</p><div className="star-ranking"><div className="star-4">★★★★★</div></div></li>
-                    <li><p>NodeJS</p><div className="star-ranking"><div className="star-3-5">★★★★★</div></div></li>
-                    <li><p>ASP.NET</p><div className="star-ranking"><div className="star-5">★★★★★</div></div></li>
+                    <li><p>Docker</p>
+                      <div className="star-ranking"></div>
+                    </li>
+                    <li><p>AWS Lambda</p>
+                      <div className="star-ranking"></div>
+                    </li>
+                    <li><p>AWS ECS/Fargate</p>
+                      <div className="star-ranking"></div>
+                    </li>
+                    <li><p>AWS CloudFormation</p>
+                      <div className="star-ranking"></div>
+                    </li>
                   </ul>
-                  <h3>Tooling</h3>
+                  <h3>Databases</h3>
                   <ul className="skills">
-                    <li><p>Heroku</p><div className="star-ranking"><div className="star-5">★★★★★</div></div></li>
-                    <li><p>AWS</p><div className="star-ranking"><div className="star-3-5">★★★★★</div></div></li>
-                    <li><p>Webpack</p><div className="star-ranking"><div className="star-4">★★★★★</div></div></li>
-                    <li><p>Git</p><div className="star-ranking"><div className="star-5">★★★★★</div></div></li>
+                    <li><p>MySQL, PostgreSQL</p>
+                      <div className="star-ranking"></div>
+                    </li>
+                    <li><p>Elastic Search</p>
+                      <div className="star-ranking"></div>
+                    </li>
+                    <li><p>Redis</p>
+                      <div className="star-ranking"></div>
+                    </li>
+                    <li><p>Neo4j, OrientDB</p>
+                      <div className="star-ranking"></div>
+                    </li>
+                    <li><p>Presto, Hive, Impala</p>
+                      <div className="star-ranking"></div>
+                    </li>
                   </ul>
                   <h3>Languages</h3>
                   <ul className="skills">
-                    <li><p>C#</p><div className="star-ranking"><div className="star-5">★★★★★</div></div></li>
-                    <li><p>Ruby</p><div className="star-ranking"><div className="star-5">★★★★★</div></div></li>
-                    <li><p>JavaScript</p><div className="star-ranking"><div className="star-5">★★★★★</div></div></li>
-                    <li><p>Java</p><div className="star-ranking"><div className="star-5">★★★★★</div></div></li>
-                    <li><p>English</p><div className="star-ranking"><div className="star-5">★★★★★</div></div></li>
+                    <li><p>Python</p>
+                      <div className="star-ranking"></div>
+                    </li>
+                    <li><p>Java</p>
+                      <div className="star-ranking"></div>
+                    </li>
+                    <li><p>C#</p>
+                      <div className="star-ranking"></div>
+                    </li>
+                    <li><p>Ruby</p>
+                      <div className="star-ranking"></div>
+                    </li>
+                    <li><p>JavaScript</p>
+                      <div className="star-ranking"></div>
+                    </li>
+                    <li><p>Friendship</p>
+                      <div className="star-ranking"></div>
+                    </li>
+                  </ul>
+                </section>
+                <section className="education-section">
+                  <h2>EDUCATION</h2>
+                  <h3>San Jose State</h3>
+                  <div className="indent">
+                    <p><b>Aug 2011 - May 2016</b></p>
+                    <p>Minor Computer Sci</p>
+                    <p>Minor Mathmatics</p>
+                    <p>B.S Civil Engineering</p>
+                  </div>
+                </section>
+                <section className="borderless">
+                  <h2>CERTIFICATES</h2>
+                  <ul className="skills">
+                    <li><p>AWS Associate Developer</p></li>
+                    <li><p>Engineer-In-Training</p></li>
+                    <li><p>Google Analytics</p></li>
                   </ul>
                 </section>
               </nav>
               <article>
                 <section>
                   <h2>PROFESSIONAL EXPERIENCE</h2>
+                  <h3>SENIOR DATA ENGINEER + TECH LEAD</h3>
+                  <h4>LEANTAAS - SANTA CLARA, CA, JUNE 2017 – PRESENT</h4>
+                  <ul>
+                    <li>Lead team of >10 engineers</li>
+                    <li>Architected data pipeline from scratch for both batch and stream data</li>
+                    <li>Designed resilient jobs to extract highly sensitive (PHI) data from hospitals</li>
+                    <li>Introduced new technologies like Apache Airflow, and AWS Athena/Lambda/Fargate with minimal
+                      supervision
+                    </li>
+                    <li>Deprecated graph (OrientDB) database and spark cluster to reduce AWS billing</li>
+                    <li>Created new data-sets for for application, PM, DS, and Data Analyst teams</li>
+                    <li>Upheld high standards with code reviews, 1-on-1s, unit and "Data Quality" tests</li>
+                    <li>At LeanTaaS data is king. The pipeline can never fail for any reason</li>
+                    <li>Wore multiple hats as engineer, product-manager, manager, and dev-ops</li>
+                  </ul>
                   <h3>FULLSTACK ENGINEER</h3>
                   <h4>DEPARTMENT OF PUBLIC WORKS – SAN FRANCISCO, CA, JUNE 2016 – MARCH 2017</h4>
                   <ul>
-                    <li>Conceptualized, designed, and built user and staff friendly automation websites </li>
-                    <li>Saved 160 weekly staff-hours by overhauling all procedures and systems to tailored digital alternatives</li>
-                  </ul>
-                  <h3>STRUCTURAL ENGINEER-IN-TRAINING</h3>
-                  <h4>UMERANI ASSOCIATES DRAFTING DEPT. – PALO ALTO, CA, JUNE 2014 – SEPTEMBER 2014</h4>
-                  <ul>
-                    <li>Modernized a library of macros for efficiency using AUTOLISP, shaving an average of 20 minutes each time a new .dwg file is created</li>
-                    <li>Demonstrated efficiency by writing scripts that computed complex calculations</li>
+                    <li>Full-Stack in ASP.NET + Entity Framework, Java Spring + Hibernate, and React</li>
+                    <li>Conceptualized, designed, and built user and staff friendly automation websites</li>
+                    <li>Saved 160 weekly staff-hours by overhauling all procedures and systems to tailored digital
+                      alternatives
+                    </li>
                   </ul>
                 </section>
                 <section className="borderless">
                   <h2>SOFTWARE ENGINEERING PROJECTS</h2>
-                  <h3>
-                    Cloud Casts 
-                    <span className="links">
-                      <a href="https://github.com/aelzeiny/CloudCasts"><i className="fa fa-github"></i></a>
-                      <a href="cloudcast.life"><i className="fa fa-external-link-square"></i></a>
-                    </span>
-                  </h3>
-                  <h4>React, Redux, Ruby on Rails, PostgreSQL, ES6</h4>
-                  <ul>
-                    <li>Developed a fully-featured podcast site that fetches live data for searching, browsing, and playing podcast episodes in real-time</li>
-                    <li>Designed an original, mobile-responsive UI/UX with modular screen layouts</li>
-                    <li>
-                      Implemented an image sampling algorithm that derives contrasting colors to dynamically generate asthetically pleasing color pallets
-                    </li>
-                  </ul>
-                  <h3>
-                    SF Subdivision Application System 
-                    <span className="links">
-                      <a href="https://github.com/aelzeiny/Subdivisionary"><i className="fa fa-github"></i></a>
-                      <a href="/#/projects/Subdivisionary"><i className="fa fa-external-link-square"></i></a>
-                    </span>
-                  </h3>
-                  <h4>ASP.NET Core, Entity Framework, Identity Framework, Code-First Workflow</h4>
-                  <ul>
-                      <li>Self-taught in ASP.NET with Identity and Entity Code-First Frameworks in 5 days</li>
-                      <li>
-                        Standardized an online application system resulting in the completion of 3 months of backlogged projects within a timespan of week and a half.
+                  <div>
+                    <h3>
+                      Apache Airflow - Open Source Contributions
+                      <span className="links">
+                <a href="https://github.com/aelzeiny/airflow/pull/1/files"><i className="fa fa-github"></i></a>
+                <a href="https://github.com/apache/airflow/pull/7030"><i className="fa fa-external-link-square"></i></a>
+              </span>
+                      <h4>Apache Airflow, AWS Fargate, Docker</h4>
+                      <ul>
+                        <li>Conceptualized the Fargate Executor as a core component to Airflow</li>
+                        <li>Created an AWS Auto-Scaling Cloud-Formation Stack which dynamically provisions new servers
+                          based on worker capacity
+                        </li>
+                      </ul>
+                    </h3>
+                  </div>
+                  <div>
+                    <h3>
+                      PHIlter, Hackaton Winner
+                      <span className="links">
+                  <a href="https://leanmachine.fastlizard4.org/MLaaS/"><i
+                      className="fa fa-external-link-square"></i></a>
+                </span>
+                    </h3>
+                    <h4>React, Redux, Flask, Fargate, SQS, API Gateways</h4>
+                    <ul>
+                      <li>Won 1st place at company hackaton for best ML product; I am not a DS</li>
+                      <li>Implemented paper on natural-language processing (NLP) to detect patient-health-information
+                        (PHI)
                       </li>
+                      <li>Aggregated data and normalized unstructured datasets</li>
+                      <li>Wrote a serverless architecture for real-time processing in AWS</li>
+                    </ul>
+                  </div>
+                  <div>
+                    <h3>
+                      Synapsis, Machine Learning Visualization
+                      <span className="links">
+                  <a href="https://github.com/ml653/synapsis" target="_blank"><i className="fa fa-github"></i></a>
+                  <a href="https://synapsis-app.herokuapp.com/" target="_blank"><i
+                      className="fa fa-external-link-square"></i></a>
+                </span>
+                    </h3>
+                    <h4>JavaScript ES6, Vue, D3, Webworker</h4>
+                    <ul>
                       <li>
-                        Converted a total of eight applications and 64 highly modular forms, all redesigned for a mobile-responsive and streamlined experience.
+                        Implemented and visualized a convolutional neural network that learns how to recognize
+                        handwriting in pure JS from from the MNIST dataset
                       </li>
-                  </ul>
-                  <h3>
-                    Synapsis, Machine Learning Visualization 
-                    <span className="links">
-                      <a href="https://github.com/ml653/synapsis" target="_blank"><i className="fa fa-github"></i></a>
-                      <a href="https://synapsis-app.herokuapp.com/" target="_blank"><i className="fa fa-external-link-square"></i></a>
-                    </span>
-                  </h3>
-                  <h4>JavaScript ES6, Vue, D3, Webworker</h4>
-                  <ul>
-                    <li>
-                      Implemented and visualized a convolutional neural network that learns how to recognize handwriting from from the MNIST dataset
-                    </li>
-                  </ul>
-                  <h3>
-                    Procedural Level Pathfinder 
-                    <span className="links">
-                      <a href="https://github.com/aelzeiny/Build-A-Mesh-Workshop" target="_blank"><i className="fa fa-github"></i></a>
-                      <a href="https://aelzeiny.github.io/Build-A-Mesh-Workshop" target="_blank"><i className="fa fa-external-link-square"></i></a>
-                    </span>
-                  </h3>
-                  <ul>
-                    <li>
-                      Implemented and depicted the Cellular Automa level generation algorithm, Polygon Addition and Subtraction algorithms,
-                      Concave Mesh generation, and A* Pathfinding
-                    </li>
-                  </ul>
-                  <h3>
-                    Data-Structures in 1000 words 
-                    <span className="links">
-                        <a href="" target="_blank"><i className="fa fa-github"></i></a>
-                        <a href="" target="_blank"><i className="fa fa-external-link-square"></i></a>
-                    </span>
-                  </h3>
-                  <ul>
-                    <li>
-                      Implemented interactive animations for 12 essential data-structures often taught in upper-division CS classes
-                    </li>
-                  </ul>
+                    </ul>
+                  </div>
+                  <div>
+                    <h3>
+                      This Resume
+                      <span className="links">
+                  <a href="https://github.com/aelzeiny/Resume/blob/master/resume.html" target="_blank"><i
+                      className="fa fa-github"></i></a>
+                  <a href="http://www.elzeiny.io" target="_blank"><i className="fa fa-external-link-square"></i></a>
+                </span>
+                    </h3>
+                    <ul>
+                      <li>
+                        Brought to you in HTML & CSS
+                      </li>
+                    </ul>
+                  </div>
                 </section>
               </article>
             </div>
